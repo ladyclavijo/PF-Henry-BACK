e6e5102038a71ee6a9c71ec548152a8cc8c68de2
Fhekz <fm.aldeco@gmail.com>
2023-05-30 03:16:47 -0300

    Get 280 books

--- src/controllers/booksControllers.js
+++ src/controllers/booksControllers.js
@@ -3,12 +3,33 @@ const axios = require("axios");
 const { API_KEY } = process.env;
 
 const getAllBooks = async () => {
-  const apiRaw = (
-    await axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${API_KEY}`
-    )
-  ).data;
-  return apiRaw;
+  let id = 0;
+  let startIndex = 0;
+  const maxResults = 40;
+  const maxBooks = 280;
+  let allBooks = [];
+
+  while (allBooks.length < maxBooks) {
+    const apiRaw = (
+      await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?maxResults=${maxResults}&startIndex=${startIndex}&q=flowers+inauthor:keyes&key=${API_KEY}`
+      )
+    ).data;
+    if (apiRaw.items) {
+      const books = apiRaw.items.map((obj) => {
+        id++;
+        return {
+          id: id,
+          name: obj.volumeInfo.title,
+          image: obj.volumeInfo.imageLinks?.thumbnail,
+          price: obj.saleInfo.listPrice?.amount,
+        };
+      });
+      allBooks.push(...books);
+    }
+    startIndex += maxResults;
+  }
+  return allBooks;
 };
 module.exports = {
   getAllBooks,
