3742f30b43bd002ec08e9b04dd0d7da7b95410ca
Agustin <Agusjua15@gmail.com>
2023-05-30 22:32:35 -0300

    agregue la ruta getAllGenres

---
+++ src/routes/genresRoutes.js
@@ -0,0 +1,7 @@
+const {Router} = require('express')
+const genresRouter = Router()
+const {getAllGenresHandler} = require('../handlers/genresHandlers')
+
+genresRouter.get('/',getAllGenresHandler)
+
+module.exports = genresRouter
