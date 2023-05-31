f123d9627c3451a5abe3ac1bab99c453ecdf3bc8
Agustin <Agusjua15@gmail.com>
2023-05-31 18:26:40 -0300

    hice la query por titulo

--- src/handlers/booksHandlers.js
+++ src/handlers/booksHandlers.js
@@ -1,9 +1,19 @@
-const { getAllBooks, getBookById } = require("../controllers/booksControllers");
+const {
+  getAllBooks,
+  getBookById,
+  getBooksByTitle,
+} = require("../controllers/booksControllers");
 
 const getAllBooksHandler = async (req, res) => {
+  const { title } = req.query;
   try {
-    const response = await getAllBooks();
-    res.status(200).json(response);
+    if (title) {
+      const response = await getBooksByTitle(title);
+      res.status(200).json(response);
+    } else {
+      const response = await getAllBooks();
+      res.status(200).json(response);
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
