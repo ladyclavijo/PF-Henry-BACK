f8d9467f400e30aa4bce7c8798242433edb02508
Agustin <Agusjua15@gmail.com>
2023-05-31 13:16:54 -0300

    cambie el force a true con la esperanza de que funcione

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 const port = process.env.PORT || 3001;
 const {inyectDbWithBooks} = require('./src/utils/utils')
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   app.listen(port, () => {
     inyectDbWithBooks()
     console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
