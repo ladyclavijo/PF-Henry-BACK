c4be4e09b39141fbff2132acb3a2fde86e354575
Fhekz <fm.aldeco@gmail.com>
2023-05-31 09:47:10 -0300

    Applying the models in the DB file

--- src/db.js
+++ src/db.js
@@ -1,6 +1,21 @@
 require("dotenv").config();
-const {URI} = process.env;
-const {Sequelize} = require('sequelize');
+const { URI } = process.env;
+const { Sequelize } = require("sequelize");
+const books = requiere("./models/Book.js");
+const authors = requiere("./models/Author.js");
+const genres = requiere("./models/Genre.js");
+const users = requiere("./models/User.js");
+
 const database = new Sequelize(URI);
 
-module.exports = {database}
+books(database);
+authors(database);
+genres(database);
+users(database);
+
+const { book, author, genre, user } = database.models;
+
+module.exports = {
+  ...database.models,
+  database,
+};
