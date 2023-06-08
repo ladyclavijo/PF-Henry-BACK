697bb58e7aac95e13146f502445a45df7fb810b6
Agustin <Agusjua15@gmail.com>
2023-06-08 15:55:48 -0300

    perdon :(

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require('cors');
+const cors = require('cors');
 const mainRouter = require("./routes/index");
 
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(morgan("dev"));
 app.use(express.json());
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 app.use("/", mainRouter);
 
 module.exports = app;
