763584797a23e75b59e189f24f6a54fa0c110bc3
Fhekz <fm.aldeco@gmail.com>
2023-06-09 19:49:13 -0300

    Update/Put Users

--- src/handlers/usersHandlers.js
+++ src/handlers/usersHandlers.js
@@ -1,4 +1,8 @@
-const { createUser, getAllUsers } = require("../controllers/usersControllers");
+const {
+  createUser,
+  getAllUsers,
+  updateUser,
+} = require("../controllers/usersControllers");
 
 const postUserHandler = async (req, res) => {
   const { id, username, name, lastname, email, country, phone } = req.body;
@@ -27,4 +31,44 @@ const getAllUsersHandler = async (req, res) => {
   }
 };
 
-module.exports = { postUserHandler, getAllUsersHandler };
+const updateUserHandler = async (req, res) => {
+  const {
+    id,
+    username,
+    name,
+    lastname,
+    email,
+    country,
+    postalcode,
+    photo,
+    phone,
+    paymentMethod,
+    shippingAddress,
+    isActive,
+    isAdmin,
+  } = req.body;
+
+  const updateData = {
+    username,
+    name,
+    lastname,
+    email,
+    country,
+    postalcode,
+    photo,
+    phone,
+    paymentMethod,
+    shippingAddress,
+    isActive,
+    isAdmin,
+  };
+
+  try {
+    const response = await updateUser(id, updateData);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { postUserHandler, getAllUsersHandler, updateUserHandler };
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,9 +3,11 @@ const userRouter = Router();
 const {
   postUserHandler,
   getAllUsersHandler,
+  updateUserHandler,
 } = require("../handlers/usersHandlers");
 
 userRouter.get("/", getAllUsersHandler);
 userRouter.post("/register", postUserHandler);
+userRouter.put("/update", updateUserHandler);
 
 module.exports = userRouter;
